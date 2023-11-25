56546d6855c81e74b490a5010d24734643936d2b
haroonwaheed-19 <haroonwaheed002@hotmail.com>
2023-11-25 15:15:57 +0500

    finding answer of base to the power of  exponent

---
+++ Recursion/power.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// power function using recursive call
+int power(int base, int exp)
+{
+    // base case
+    if (exp == 0)
+    {
+        return 1;
+    }
+    // recursive call
+    return base * power(base, exp - 1);
+}
+int main()
+{
+    int base, exp;
+    cout << "\nEnter base : ";
+    cin >> base;
+    cout << "\nEnter exponent : ";
+    cin >> exp;
+    int ans = power(base, exp);
+    cout << "\nPower of " << base << " to power " << exp << " is : " << ans << endl;
+    return 0;
+}

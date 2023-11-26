2491ec5111427cc634d8695f08af9f0edb41fd94
haroonwaheed-19 <haroonwaheed002@hotmail.com>
2023-11-26 10:59:15 +0500

    Update insertionInArrayAtSpecific.cpp

--- Arrays/insertionInArrayAtSpecific.cpp
+++ Arrays/insertionInArrayAtSpecific.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void print(int *arr, int n)
 {
-    cout << "\nElements in the array are : ";
+    cout << "\n\tElements in the array are : ";
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
@@ -13,7 +13,7 @@ void insertAtSpecific(int *arr, int &n, int index, int key)
 {
     int j = n - 1;
     n = n + 1;
-    while (j >=index)
+    while (j >= index)
     {
         arr[j + 1] = arr[j];
         j--;
@@ -25,8 +25,8 @@ int main()
     int n = 5;
     int arr[n] = {2, 5, 10, 15, 52};
     print(arr, n);
-    cout << "\n\t\tAfter Insertion" << endl;
+    cout << "\n\t\t\tAfter Insertion" << endl;
     insertAtSpecific(arr, n, 3, 12);
-    print(arr, n);// 2 5 10 12 15 52
+    print(arr, n); // 2 5 10 12 15 52
     return 0;
 }

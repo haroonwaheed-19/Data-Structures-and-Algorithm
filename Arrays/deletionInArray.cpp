6442800a8db3a41cce0222f5aa9fc8e5c4b8fa72
haroonwaheed-19 <haroonwaheed002@hotmail.com>
2023-11-26 10:58:12 +0500

    Update deletionInArray.cpp

--- Arrays/deletionInArray.cpp
+++ Arrays/deletionInArray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void print(int *arr, int n)
 {
-    cout << "\nElements in the array are : ";
+    cout << "\n\tElements in the array are : ";
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
@@ -22,7 +22,7 @@ void deleteAtSpecific(int *arr, int &n, int index)
 int main()
 {
     int n = 6;
-    int arr[n] = {2, 5, 10, 15, 52,60};
+    int arr[n] = {2, 5, 10, 15, 52, 60};
     print(arr, n);
     cout << "\n\t\tAfter Deletion" << endl;
     deleteAtSpecific(arr, n, 3);
